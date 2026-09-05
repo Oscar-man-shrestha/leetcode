d86cd9d97bbac8ab96270acf1da636af3f10dfb7
Oscar Man Shrestha <bl.sc.u4cse24267@bl.students.amrita.edu>
2026-09-05 18:39:05 +0530

    Time: 2 ms (11.58%), Space: 46.2 MB (88.21%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
@@ -1,20 +1,27 @@
-public class Solution {
-
+class Solution {
     public int firstStableIndex(int[] nums, int k) {
-        int n = nums.length;
-        for (int i = 0; i < n; i++) {
-            int maxValue = nums[i];
-            int minValue = nums[i];
-            for (int j = 0; j < i; j++) {
-                maxValue = Math.max(maxValue, nums[j]);
+        // brute force approach?
+        int maxi = nums[0];
+        int mini = Integer.MAX_VALUE;
+        int ans = Integer.MAX_VALUE;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>maxi){
+                maxi = nums[i];
             }
-            for (int j = i + 1; j < n; j++) {
-                minValue = Math.min(minValue, nums[j]);
+            mini = nums [i];
+            
+            for(int j=i;j<nums.length;j++){
+                if(mini>nums[j]){
+                    mini=nums[j];
+                }
             }
-            if (maxValue - minValue <= k) {
-                return i;
+            if((maxi-mini)<=k){
+                ans = Math.min(ans,i);
             }
-        }
-        return -1;
+            
+        }if(ans == Integer.MAX_VALUE){ans = -1;}
+        return ans;
+
+
     }
 }
